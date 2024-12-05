c0b7da8e059d3a6cce277dd033f58c3f56934852
John Doe <johndoe@example.com>
2024-12-05 22:53:42 +0100

    implemented auth components

---
+++ app/signup/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Signup from '../../components/auth-components/signup';
+
+const SignupPage: React.FC = () => {
+  return (
+    <div>
+      <Signup />
+    </div>
+  );
+};
+
+export default SignupPage;
